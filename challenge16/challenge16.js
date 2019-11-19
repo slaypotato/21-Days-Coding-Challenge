7ff122af4bf06223458981b6834d3303ceb9dc5f
Rafael Morales <rafael_c_morales@yahoo.com.br>
2019-11-19 13:37:31 -0800

    Update challenge16.js

--- challenge16/challenge16.js
+++ challenge16/challenge16.js
@@ -1,5 +1,4 @@
 const festivalColours = color1 => {
-  // code here!
   /*
     A split-complementary color scheme is a three-color combination that consists of a base color (H0) and two colors (H1 and H2) that are 150 degrees and 210 degrees apart from H0 respectively.
     formula: H1 = |(H0 + 150 degrees) - 360 degrees|
