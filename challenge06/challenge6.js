70e46dcadf9465f1f2984d83c996688e6e42d771
Rafael Morales <rafael_c_morales@yahoo.com.br>
2019-11-19 12:44:52 -0800

    Update challenge6.js

--- challenge06/challenge6.js
+++ challenge06/challenge6.js
@@ -1,12 +1,11 @@
 
 const voterTurnout = (voter_signatures, voter_ids) => {
-  // Code here!
+  
   if (voter_signatures.length !== voter_ids.length) return false;
   for (var i = 0; i < voter_signatures.length; i++){
     if (voter_signatures[i] !== voter_ids[i]) return "FRAUD!";
   }
   return "All clear, we can count the votes!";
-
 }
  /*
  
