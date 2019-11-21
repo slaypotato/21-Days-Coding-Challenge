6bcfdd972921b10aaf73c335dc546d4b8d8f47d3
Rafael Morales <rafael_c_morales@yahoo.com.br>
2019-11-21 10:00:11 -0800

    Add files via upload

---
+++ challenge21/challenge21.js
@@ -0,0 +1,50 @@
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  // Code here!
+  var recipesA = checkBakery(bakeryA,recipes);
+  var recipesB = checkBakery(bakeryB,recipes);
+  
+  for (var recipe of recipesA){
+    if (recipesB.includes(recipe)){
+      return recipe;
+    }
+  }
+  return 'no recipe found'
+}
+
+const checkBakery = function (bakery, recipes){
+  var validRecipes = [];
+  for(var ingredient of bakery){
+    for (var recipe of recipes){
+      if (recipe['ingredients'].includes(ingredient)){
+        validRecipes.push(recipe['name']);
+      }
+    }
+  }
+  return validRecipes;
+}
+
+/*
+
+Input:
+
+    const bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard']
+    const bakeryB = ['milk', 'butter', 'cream cheese']
+    const recipes = [
+        {
+            name: 'Coconut Sponge Cake',
+            ingredients: ['coconut', 'cake base']
+        },
+        {
+            name: 'Persian Cheesecake',
+            ingredients: ['saffron', 'cream cheese']
+        },
+        {
+            name: 'Custard Surprise',
+            ingredients: ['custard', 'ground beef']
+        }
+    ]
+    
+Output:
+Persian Cheesecake
+
+*/
